57a6c9a638d472b5417aac6403b879c21b0fb7d5
FrancescoMarelli <alu0101161730@ull.edu.es>
2024-06-06 23:54:09 +0100

    posenet detect but get crazy

--- src/pose-tracker-engine/pose-tracker.ts
+++ src/pose-tracker-engine/pose-tracker.ts
@@ -5,12 +5,15 @@ import Camera from '~/pose-tracker-engine/camera';
 import {
   IPoseTrackerRenderElementsSettings,
 } from '~/pose-tracker-engine/types/pose-tracker-dender-elements-settings.interface';
+import PosenetDetector from '~/pose-tracker-engine/types/adaptadores/posenet-detector';
+import { mapKeypointsToLandmarks } from '~/pose-tracker-engine/utils';
 import { PoseDetector } from '~/pose-tracker-engine/types/adaptadores/pose-detector.interface';
 import { MediapipePoseDetector } from '~/pose-tracker-engine/types/adaptadores/mediapipe-pose-detector';
 
 export default class PoseTracker {
-  private pose: PoseDetector;
+  private pose: PosenetDetector;
   private camera: Camera | null;
+  private videoEl: HTMLVideoElement | null;
 
   constructor(
     videoEl: HTMLVideoElement | null,
@@ -22,19 +25,31 @@ export default class PoseTracker {
       videoEl.id = 'input-video';
     }
 
-    // This is for clearing any existing cache from the video element
     videoEl.pause();
     videoEl.removeAttribute('src');
     videoEl.load();
 
-    this.pose = new MediapipePoseDetector();
-
+    this.pose = new PosenetDetector();
+/*  this.pose = new MediapipePoseDetector();
     this.pose.setOptions(settings);
     this.pose.onResults(onResults);
     this.camera = new Camera(
       videoEl,
       async (): Promise<void> => {
         await this.pose.getPose()?.send({ image: videoEl });
+      },
+    );*/
+    this.camera = new Camera(
+      videoEl,
+      async (): Promise<void> => {
+        if (videoEl) {
+            const pose = await this.pose.estimatePose(videoEl);
+          if (pose) {
+            const landmarks = mapKeypointsToLandmarks(pose);
+            const results: IPoseTrackerResults = { image: videoEl, poseLandmarks: landmarks };
+            onResults(results);
+          }
+        }
       },
     );
     this.camera.start(settings.width, settings.height);
@@ -44,7 +59,7 @@ export default class PoseTracker {
     this.pose.shutdown();
     this.camera?.stop();
     this.camera = null;
-    this.pose.close();
+    //this.pose.close();
     // @ts-ignore
     this.pose = null;
   }
@@ -54,6 +69,8 @@ export default class PoseTracker {
     results: IPoseTrackerResults,
     renderElementsSettings?: IPoseTrackerRenderElementsSettings,
   ): void {
-    this.pose.drawResults(ctx,results,renderElementsSettings);
+    if (renderElementsSettings) {
+      this.pose.drawResults(ctx, results, renderElementsSettings);
+    }
   }
 }
---
+++ src/pose-tracker-engine/types/adaptadores/posenet-detector.ts
@@ -0,0 +1,108 @@
+import * as posenet from '@tensorflow-models/posenet';
+import { PoseNet } from '@tensorflow-models/posenet';
+import * as tf from '@tensorflow/tfjs-core';
+import '@tensorflow/tfjs-backend-webgl';
+import { IPoseTrackerResults } from '~/pose-tracker-engine/types/pose-tracker-results.interface';
+import {
+  IPoseTrackerRenderElementsSettings,
+} from '~/pose-tracker-engine/types/pose-tracker-dender-elements-settings.interface';
+import { flipPoseHorizontal } from '@tensorflow-models/posenet/dist/util';
+import { MediapipePoseDetector } from '~/pose-tracker-engine/types/adaptadores/mediapipe-pose-detector';
+import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils/drawing_utils';
+import { POSE_CONNECTIONS } from '@mediapipe/pose/pose';
+
+
+export default class PosenetDetector {
+  private net: posenet.PoseNet | null = null;
+  private modelLoaded: Promise<void>;
+  private width = 1280;
+  private height = 720;
+
+
+  constructor() {
+    tf.setBackend('webgl'); // or 'wasm' or 'cpu'
+    this.modelLoaded = this.loadModel();
+  }
+
+  private async loadModel() {
+    this.net = await posenet.load({
+      architecture: 'MobileNetV1',
+      outputStride: 16,
+      inputResolution: { width: this.width, height: this.height },
+      multiplier: 0.75
+    });
+  }
+
+  public getPose(): PoseNet | null {
+    return this.net;
+  }
+
+  public async estimatePose(video: HTMLVideoElement): Promise<posenet.Pose | null> {
+  //  await this.modelLoaded; // Asegúrate de que el modelo esté cargado
+    return new Promise(async (resolve, reject) => {
+      // Espera a que el video esté listo antes de estimar la pose
+      if (this.net) {
+        video.setAttribute('width', this.width.toString());
+        video.setAttribute('height', this.height.toString());
+        let pose = await this.net.estimateSinglePose(video, {
+          flipHorizontal: false
+        });
+       // flipPoseHorizontal(pose, this.width);
+
+      if (pose) {
+              // Escalar las coordenadas de los landmarks al tamaño del canvas
+          const scaledLandmarks = pose.keypoints.map((keypoint) => {
+            return {
+              ...keypoint,
+              position: {
+                x: keypoint.position.x / 1280,
+                y: keypoint.position.y / 720
+              }
+            };
+          });
+
+          resolve({
+            ...pose,
+            keypoints: scaledLandmarks
+          });
+        }
+       resolve(null);
+      };
+
+      // Si hay un error al cargar el video, rechaza la promesa
+      video.onerror = (error) => {
+        reject(error);
+      };
+    });
+  }
+
+  public shutdown(): void {
+    this.net = null;
+  }
+
+  public drawResults(
+    ctx: CanvasRenderingContext2D,
+    results: IPoseTrackerResults,
+    renderElementsSettings: IPoseTrackerRenderElementsSettings
+  ): void {
+    if (!results || !ctx) {
+      return;
+    }
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    if (renderElementsSettings.shouldDrawFrame && results.image) {
+      ctx.drawImage(results.image, 0, 0, ctx.canvas.width, ctx.canvas.height);
+    }
+
+    if (renderElementsSettings.shouldDrawPoseLandmarks && results.poseLandmarks) {
+      ctx.save();
+      if(MediapipePoseDetector.showLandmarks) {
+        drawConnectors(ctx, results.poseLandmarks, POSE_CONNECTIONS, { color: '#c4c4c4', lineWidth: 4 });
+        drawLandmarks(ctx, results.poseLandmarks, { color: '#0051ff', lineWidth: 2 });
+      }
+      ctx.restore();
+
+    }
+  }
+}
